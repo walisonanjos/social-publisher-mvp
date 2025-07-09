681d4b6729ef962419de3eeebb2210f76e8f3b19
walison anjos <walison.anjos@gmail.com>
2025-07-09 12:30:31 +0000

    fix(lint): corrigi nextconfig para liberar thumbails

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
+// next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
