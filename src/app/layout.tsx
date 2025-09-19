7c7feb676a362f94880925d0f12da7bce6113b4e
walison anjos <walison.anjos@gmail.com>
2025-09-19 17:52:14 +0000

    feat: Adiciona I18nextProvider e configura i18n na aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/Footer";
-import { Toaster } from 'sonner'; // <-- 1. IMPORTAR O TOASTER
+import { Toaster } from 'sonner';
+import { I18nextProvider } from "react-i18next"; // <-- IMPORTANDO O PROVEDOR
+import i18n from "../i18n"; // <-- IMPORTANDO A CONFIGURAÇÃO
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,17 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-gray-900`}
-      >
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
-        {/* 2. ADICIONAR O COMPONENTE AQUI */}
-        <Toaster theme="dark" richColors position="bottom-right" />
-      </body>
-    </html>
+    <I18nextProvider i18n={i18n}>
+      <html lang={i18n.language}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-gray-900`}
+        >
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer />
+          <Toaster theme="dark" richColors position="bottom-right" />
+        </body>
+      </html>
+    </I18nextProvider>
   );
 }
